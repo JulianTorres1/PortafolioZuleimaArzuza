983c14a326c895d2abb34514f0e2d5dea4af55a6
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2024-12-30 19:24:06 -0500

    logo cambiado y agregadas el resto de la obras

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -23,6 +23,51 @@ import INCOGNITA4 from '../media/img/Obras/INCOGNITA4.jpg'
 import INCOGNITA5 from '../media/img/Obras/INCOGNITA5.jpg'
 import POPA from '../media/img/Obras/POPA.jpg'
 
+// Obras en acrílico con intervención
+import CHANGO from '../media/img/Obras/CHANGO.jpeg'
+import OSHUN from '../media/img/Obras/OSHUN.jpeg'
+import OBATALA from '../media/img/Obras/OBATALA.jpeg'
+import YEMAYA from '../media/img/Obras/YEMAYA.jpg'
+import GUARDIAN from '../media/img/Obras/GUARDIAN.jpeg'
+import LATIDOS from '../media/img/Obras/LATIDOS.jpeg'
+
+// Obras en acuarela
+import VALLE from '../media/img/Obras/VALLE.jpg'
+import MARNIE from '../media/img/Obras/MARNIE.jpg'
+
+// Obras en lápiz
+import OFELIA from '../media/img/Obras/OFELIA.jpg'
+import CARLOS from '../media/img/Obras/CARLOS.jpg'
+import BRAYAN from '../media/img/Obras/BRAYAN .jpg'
+import MAT from '../media/img/Obras/MAT.jpg'
+import MABELYN from '../media/img/Obras/MABELYN.jpg'
+import MITAD from '../media/img/Obras/MITAD.jpg'
+import MARY from '../media/img/Obras/MARY.jpg'
+import PULPO from '../media/img/Obras/PULPO.jpg'
+
+// Obras en color
+import EXPRESIONES1 from '../media/img/Obras/EXPRESIONES1 .jpg'
+import EXPRESIONES2 from '../media/img/Obras/EXPRESIONES2.jpg'
+import EXPRESIONES3 from '../media/img/Obras/EXPRESIONES3.jpg'
+import RASTRAS from '../media/img/Obras/RASTRAS.jpg'
+import MEDUSAS from '../media/img/Obras/MEDUSAS.jpg'
+import INFANCIA from '../media/img/Obras/INFANCIA.jpg'
+
+// Obras digitales
+import FORZADAS from '../media/img/Obras/FORZADAS.jpg'
+import FANTASTICA from '../media/img/Obras/FANTASTICA.jpg'
+import VENTA from '../media/img/Obras/VENTA.jpg'
+import MILK from '../media/img/Obras/MILK.jpg'
+
+// Obras en fotografía
+import SUENO from '../media/img/Obras/SUENO.jpg'
+import GALA from '../media/img/Obras/GALA.jpg'
+import SOGA from '../media/img/Obras/SOGA.jpg'
+import ANONIMO from '../media/img/Obras/ANONIMO.jpg'
+import ORLANDO from '../media/img/Obras/ORLANDO.jpg'
+import LABOR from '../media/img/Obras/LABOR.jpg'
+import ESQUINA from '../media/img/Obras/ESQUINA.jpg'
+import FACHADA from '../media/img/Obras/FACHADA.jpg'
 
 const artworks = [
       { id: '1', title: 'OSADA', description: '36x51 cm, óleo sobre lienzo', imageUrl: OSADA, category: 'Óleo', date: '2021' },
@@ -47,50 +92,50 @@ const artworks = [
       { id: '18', title: 'LA POPA', description: '12x9 cm, acrílico sobre lienzo', imageUrl: POPA, category: 'Acrílico', date: '2021' },
       
       // Acrílico con intervención
-      { id: '19', title: 'CHANGO: LATIDOS DE PASION Y VALOR', description: '80x50 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2023' },
-      { id: '20', title: 'OSHUN: REFLEJOS DORADOS DE AMOR Y ABUNDANCIA', description: '80x50 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2023' },
-      { id: '21', title: 'OBATALÀ: SERENIDAD Y SABIDURIA EN LA CREACION', description: '80x50 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2023' },
-      { id: '22', title: 'YEMAYÀ: OLEAJE DE SERENIDAD Y PROTECCIÒN', description: '80x50 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2023' },
-      { id: '23', title: 'LATIDOS DEL MAR', description: '30x40 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2024' },
-      { id: '24', title: 'EL GUARDIAN', description: '30x40 cm, acrílico con intervención', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acrílico con intervención', date: '2024' },
+      { id: '19', title: 'CHANGO: LATIDOS DE PASION Y VALOR', description: '80x50 cm, acrílico con intervención', imageUrl: CHANGO, category: 'Acrílico con intervención', date: '2023' },
+      { id: '20', title: 'OSHUN: REFLEJOS DORADOS DE AMOR Y ABUNDANCIA', description: '80x50 cm, acrílico con intervención', imageUrl: OSHUN, category: 'Acrílico con intervención', date: '2023' },
+      { id: '21', title: 'OBATALÀ: SERENIDAD Y SABIDURIA EN LA CREACION', description: '80x50 cm, acrílico con intervención', imageUrl: OBATALA, category: 'Acrílico con intervención', date: '2023' },
+      { id: '22', title: 'YEMAYÀ: OLEAJE DE SERENIDAD Y PROTECCIÒN', description: '80x50 cm, acrílico con intervención', imageUrl: YEMAYA, category: 'Acrílico con intervención', date: '2023' },
+      { id: '23', title: 'LATIDOS DEL MAR', description: '30x40 cm, acrílico con intervención', imageUrl: LATIDOS, category: 'Acrílico con intervención', date: '2024' },
+      { id: '24', title: 'EL GUARDIAN', description: '30x40 cm, acrílico con intervención', imageUrl: GUARDIAN, category: 'Acrílico con intervención', date: '2024' },
       
       // obras en acuarela
-      { id: '25', title: 'VALLE DEL VIENTO', description: '20x14 cm, acuarela', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acuarela', date: '2020' },
-      { id: '26', title: 'MARNIE', description: '20x14 cm, acuarela', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Acuarela', date: '2020' },
+      { id: '25', title: 'VALLE DEL VIENTO', description: '20x14 cm, acuarela', imageUrl: VALLE, category: 'Acuarela', date: '2020' },
+      { id: '26', title: 'MARNIE', description: '20x14 cm, acuarela', imageUrl: MARNIE, category: 'Acuarela', date: '2020' },
 
       // Obras en lápiz
-      { id: '27', title: 'OFELIA', description: '21x28 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2019' },
-      { id: '28', title: 'CARLOS', description: '25x35 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2019' },
-      { id: '29', title: 'BRAYAN', description: '21x28 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2021' },
-      { id: '30', title: 'MAT', description: '21x28 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2019' },
-      { id: '31', title: 'MABELYN', description: '21x28 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2020' },
-      { id: '32', title: 'MITAD', description: '25x35 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2019' },
-      { id: '33', title: 'A LA INVERSA', description: '21x28 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2021' },
-      { id: '34', title: 'MARY', description: '30x50 cm, lápiz', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Lápiz', date: '2021' },
+      { id: '27', title: 'OFELIA', description: '21x28 cm, lápiz', imageUrl: OFELIA, category: 'Lápiz', date: '2019' },
+      { id: '28', title: 'CARLOS', description: '25x35 cm, lápiz', imageUrl: CARLOS, category: 'Lápiz', date: '2019' },
+      { id: '29', title: 'BRAYAN', description: '21x28 cm, lápiz', imageUrl: BRAYAN, category: 'Lápiz', date: '2021' },
+      { id: '30', title: 'MAT', description: '21x28 cm, lápiz', imageUrl: MAT, category: 'Lápiz', date: '2019' },
+      { id: '31', title: 'MABELYN', description: '21x28 cm, lápiz', imageUrl: MABELYN, category: 'Lápiz', date: '2020' },
+      { id: '32', title: 'MITAD', description: '25x35 cm, lápiz', imageUrl: MITAD, category: 'Lápiz', date: '2019' },
+      { id: '33', title: 'A LA INVERSA', description: '21x28 cm, lápiz', imageUrl: PULPO, category: 'Lápiz', date: '2021' },
+      { id: '34', title: 'MARY', description: '30x50 cm, lápiz', imageUrl: MARY, category: 'Lápiz', date: '2021' },
 
       // Obras en color
-      { id: '35', title: 'MI EXPRESION 1/3', description: '28x21 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2020' },
-      { id: '36', title: 'MI EXPRESION 2/3', description: '28x21 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2020' },
-      { id: '37', title: 'MI EXPRESION 3/3', description: '28x21 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2020' },
-      { id: '38', title: 'A RASTRAS', description: '14x29 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2020' },
-      { id: '39', title: 'BAILANDO EN MEDUSAS', description: '14x29 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2020' },
-      { id: '40', title: 'INFANCIA', description: '28x21 cm, color', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Color', date: '2019' },
+      { id: '35', title: 'MI EXPRESION 1/3', description: '28x21 cm, color', imageUrl: EXPRESIONES1, category: 'Color', date: '2020' },
+      { id: '36', title: 'MI EXPRESION 2/3', description: '28x21 cm, color', imageUrl: EXPRESIONES2, category: 'Color', date: '2020' },
+      { id: '37', title: 'MI EXPRESION 3/3', description: '28x21 cm, color', imageUrl: EXPRESIONES3, category: 'Color', date: '2020' },
+      { id: '38', title: 'A RASTRAS', description: '14x29 cm, color', imageUrl: RASTRAS, category: 'Color', date: '2020' },
+      { id: '39', title: 'BAILANDO EN MEDUSAS', description: '14x29 cm, color', imageUrl: MEDUSAS, category: 'Color', date: '2020' },
+      { id: '40', title: 'INFANCIA', description: '28x21 cm, color', imageUrl: INFANCIA, category: 'Color', date: '2019' },
 
       // Obras digitales
-      { id: '41', title: 'FORZADAS', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2021' },
-      { id: '42', title: 'RITMO Y SABOR EN LA FANTASTICA', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2024' },
-      { id: '43', title: 'APATIA EN VENTA', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2021' },
-      { id: '44', title: 'MILK', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2021' },
-      { id: '45', title: 'UN SUEÑO', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2020' },
-      { id: '46', title: 'GALA', description: 'Digital', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Digital', date: '2020' },
+      { id: '41', title: 'FORZADAS', description: 'Digital', imageUrl: FORZADAS, category: 'Digital', date: '2021' },
+      { id: '42', title: 'RITMO Y SABOR EN LA FANTASTICA', description: 'Digital', imageUrl: FANTASTICA, category: 'Digital', date: '2024' },
+      { id: '43', title: 'APATIA EN VENTA', description: 'Digital', imageUrl: VENTA, category: 'Digital', date: '2021' },
+      { id: '44', title: 'MILK', description: 'Digital', imageUrl: MILK, category: 'Digital', date: '2021' },
 
       // Obras en fotografía
-      { id: '47', title: 'CON LA SOGA AL CUELLO', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' },
-      { id: '48', title: 'ANONIMO', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' },
-      { id: '49', title: 'LABOR', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' },
-      { id: '50', title: 'ORLANDO', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' },
-      { id: '51', title: 'ESQUINA', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' },
-      { id: '52', title: 'FACHADA', description: 'Fotografía', imageUrl: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912', category: 'Fotografía', date: '2019' }
+      { id: '45', title: 'UN SUEÑO', description: 'Fotografía Digital', imageUrl: SUENO, category: 'Fotografía', date: '2020' },
+      { id: '46', title: 'GALA', description: 'Fotografía Digital', imageUrl: GALA, category: 'Fotografía', date: '2020' },
+      { id: '47', title: 'CON LA SOGA AL CUELLO', description: 'Fotografía Digital', imageUrl: SOGA, category: 'Fotografía', date: '2019' },
+      { id: '48', title: 'ANONIMO', description: 'Fotografía Digital', imageUrl: ANONIMO, category: 'Fotografía', date: '2019' },
+      { id: '49', title: 'LABOR', description: 'Fotografía Digital', imageUrl: LABOR, category: 'Fotografía', date: '2019' },
+      { id: '50', title: 'ORLANDO', description: 'Fotografía Digital', imageUrl: ORLANDO, category: 'Fotografía', date: '2019' },
+      { id: '51', title: 'ESQUINA', description: 'Fotografía Digital', imageUrl: ESQUINA, category: 'Fotografía', date: '2019' },
+      { id: '52', title: 'FACHADA', description: 'Fotografía Digital', imageUrl: FACHADA, category: 'Fotografía', date: '2019' }
 
 ];
 
@@ -117,6 +162,51 @@ export default function Gallery() {
             <ArtworkCard key={artwork.id} artwork={artwork} />
           ))}
         </div>
+
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Acrílico con intervención</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Acrílico con intervención').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+        
+        
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Acuarela</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Acuarela').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+        
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Lápiz</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Lápiz').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Color</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Color').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Digital</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Digital').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+
+        <h2 className="mb-20 mt-20 text-center text-3xl text-gray-900 font-light border-b border-primary-300">Fotografía</h2>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {artworks.filter((artwork) => artwork.category === 'Fotografía').map((artwork) => (
+            <ArtworkCard key={artwork.id} artwork={artwork} />
+          ))}
+        </div>
+        
+        
       </div>
     </>
   );
